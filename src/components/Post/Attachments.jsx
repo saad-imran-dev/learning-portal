9a1d595e28bde169b7fdcee12a3f4da3998b9bef
Muhmmad Saad <saad.imran.vohra@gmail.com>
2023-11-12 16:10:54 +0500

    Pages and Components folders

---
+++ src/components/Post/Attachments.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+function Attachments() {
+    return (
+        <Typography>Attachments</Typography>
+    )
+}
+
+export default Attachments
---
+++ src/pages/Post.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Typography, Box } from '@mui/material'
+import Attachments from '../components/Post/Attachments'
+
+function Post() {
+    return (
+        <Box>
+            <Typography>Post</Typography>
+            <Attachments />
+        </Box>
+    )
+}
+
+export default Post
