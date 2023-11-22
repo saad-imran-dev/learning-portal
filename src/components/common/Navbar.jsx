29ddfd01764d7dae69580d6aa67a07c4385c7332
AliHM15 <91543588+AliHM15@users.noreply.github.com>
2023-11-22 19:22:17 +0500

    I have created CreatePost page and all the apis for this part are working

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,30 +1,42 @@
-import React from 'react'
-import { Avatar, Box, Button, Paper, Typography } from '@mui/material'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Avatar, Box, Button, Paper, Typography } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
-    return (
-        <Paper sx={{
-            height: '10vh',
-            width: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            gap: 3,
-            px: 3,
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            zIndex: 10,
-        }} >
-            <Typography sx={{ mr: 'auto' }}>Navbar</Typography>
-            <Button color='primary' variant='contained' >
-                <FontAwesomeIcon icon={faPlus} style={{ marginRight: '5px' }} />
-                Create Post
-            </Button>
-            <Avatar />
-        </Paper>
-    )
+    const navigate = useNavigate();
+
+    const handlePostClick = () => {
+        navigate("/post/create");
+      };
+
+  return (
+    <Paper
+      sx={{
+        height: "10vh",
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        gap: 3,
+        px: 3,
+        position: "absolute",
+        top: 0,
+        left: 0,
+        zIndex: 10,
+      }}
+    >
+      <Typography sx={{ mr: "auto" }}>Navbar</Typography>
+      <Button color="primary" variant="contained" onClick={handlePostClick}>
+        <FontAwesomeIcon
+          icon={faPlus}
+          style={{ marginRight: "5px" }}
+        />
+        Create Post
+      </Button>
+      <Avatar />
+    </Paper>
+  );
 }
 
-export default Navbar
+export default Navbar;
